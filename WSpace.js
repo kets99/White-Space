7e145a5385b9e7a192b927783d4ecb05ca6b30d5
kets99 <davdaketki@gmail.com>
2018-03-10 13:35:18 +0530

    First commit

---
+++ WSpace.js
@@ -0,0 +1,24 @@
+var c = document.getElementById("myCanvas");
+var ctx = c.getContext("2d");
+ctx.moveTo(0,0);
+ctx.lineTo(200,100);
+ctx.stroke();/*var drawing =( function() {
+		var context,
+			canvas;
+
+
+    init= function ()	
+
+	{
+	
+		canvas=document.getElementById('whitecanvas');
+
+		context=document.getElementById('whitecanvas').getContext("2d");
+		context.beginPath(); 
+		context.font = "30px Arial";
+		context.fillText("Hello World",100,50);
+  	}
+
+	}
+
+	)*/
